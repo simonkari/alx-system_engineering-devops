23e5b57ee14ce6897bbfe88dbda9bcccae2ab81b
simonkari <kariithisimon1715@gmail.com>
2023-07-04 19:35:13 +0300

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,6 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+
+input = ARGV[0]
+matches = input.scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
+result = matches.join(",")
+puts result
