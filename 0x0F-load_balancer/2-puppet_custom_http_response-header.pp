b74682ec5b168584d87777518a71d92a858524bd
simonkari <kariithisimon1715@gmail.com>
2023-09-04 22:43:41 +0300

    2-puppet_custom_http_response-header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -4,15 +4,18 @@ exec { '/usr/bin/env apt-get -y update' : }
 -> package { 'nginx':
   ensure => installed,
 }
+
 -> file { '/var/www/html/index.html' :
   content => 'Holberton School!',
 }
+
 -> file_line { 'add header' :
   ensure => present,
   line   => "\tadd_header X-Served-By ${hostname};",
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
 }
+
 -> service { 'nginx':
   ensure => running,
 }
