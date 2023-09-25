e2d3f6a6190f4c03c747703cfe6f4782261a5220
simonkari <kariithisimon1715@gmail.com>
2023-09-25 19:06:31 +0300

    Gathering data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,27 +16,27 @@ def main():
         if re.fullmatch(r'\d+', sys.argv[1]):
             # Convert the argument to an integer (user ID)
             user_id = int(sys.argv[1])
-            
+
             # Fetch user data from the API
             user_res = requests.get(f'{API_URL}/users/{user_id}').json()
-            
+
             # Fetch all todos from the API
             todos_res = requests.get(f'{API_URL}/todos').json()
-            
+
             # Extract the user's name from the user data
             user_name = user_res.get('name')
-            
+
             # Filter todos by the user's ID
             todos = [todo for todo in todos_res if todo.get('userId') == user_id]
-            
+
             # Filter completed todos
             todos_done = [todo for todo in todos if todo.get('completed')]
-            
+
             # Display the results
             print(
                 f'Employee {user_name} is done with tasks ({len(todos_done)}/{len(todos)}):'
             )
-            
+
             # Print the titles of completed todos
             for todo_done in todos_done:
                 print(f'\t{todo_done.get("title")}')
