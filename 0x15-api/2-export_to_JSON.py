0fa9359e51e3f4ea844ed7ec8e97d48776a51f5f
simonkari <kariithisimon1715@gmail.com>
2023-09-25 19:40:11 +0300

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -17,19 +17,19 @@ if __name__ == '__main__':
         if re.fullmatch(r'\d+', sys.argv[1]):
             # Parse the user ID from the command-line argument
             id = int(sys.argv[1])
-            
+
             # Fetch user data from the API
             user_res = requests.get(f'{API_URL}/users/{id}').json()
-            
+
             # Fetch todos data from the API
             todos_res = requests.get(f'{API_URL}/todos').json()
-            
+
             # Extract the username of the user
             user_name = user_res.get('username')
-            
+
             # Filter todos for the given user ID
             todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            
+
             # Create a JSON file with user and todos data
             with open(f'{id}.json', 'w') as file:
                 user_data = list(map(
