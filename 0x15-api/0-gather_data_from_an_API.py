c1e930a0a814f66948158ab43ffda8e9c385337f
simonkari <kariithisimon1715@gmail.com>
2023-09-25 19:11:17 +0300

    Gathering data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,45 +1,41 @@
 #!/usr/bin/python3
-'''A script that gathers data from an API.
+'''
+A script that gathers data from an API.
 '''
 
-import re
 import requests
+import re
 import sys
+
 
 # The URL of the API we are querying
 API_URL = 'https://jsonplaceholder.typicode.com'
+'''
+The URL of the API.
+'''
 
-def main():
-    # Check if a command-line argument is provided
-    if len(sys.argv) > 1:
-        # Check if the provided argument is a positive integer
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            # Convert the argument to an integer (user ID)
-            user_id = int(sys.argv[1])
-
-            # Fetch user data from the API
-            user_res = requests.get(f'{API_URL}/users/{user_id}').json()
-
-            # Fetch all todos from the API
-            todos_res = requests.get(f'{API_URL}/todos').json()
-
-            # Extract the user's name from the user data
-            user_name = user_res.get('name')
-
-            # Filter todos by the user's ID
-            todos = [todo for todo in todos_res if todo.get('userId') == user_id]
-
-            # Filter completed todos
-            todos_done = [todo for todo in todos if todo.get('completed')]
-
-            # Display the results
-            print(
-                f'Employee {user_name} is done with tasks ({len(todos_done)}/{len(todos)}):'
-            )
-
-            # Print the titles of completed todos
-            for todo_done in todos_done:
-                print(f'\t{todo_done.get("title")}')
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            # Fetch user data from the API
+            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
+            # Fetch all todos from the API
+            todos_res = requests.get('{}/todos'.format(API_URL)).json()
+            # Extract the user's name from the user data
+            user_name = user_res.get('name')
+            # Filter todos by the user's ID
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            # Filter completed todos
+            todos_done = list(filter(lambda x: x.get('completed'), todos))
+            
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    user_name,
+                    len(todos_done),
+                    len(todos)
+                )
+            )
+            for todo_done in todos_done:
+                print('\t {}'.format(todo_done.get('title')))
