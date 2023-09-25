6fa6c1736ddd75991b776b0383a68d1cd725c8c3
simonkari <kariithisimon1715@gmail.com>
2023-09-25 19:44:11 +0300

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-'''Gathers data from an API and exports it to a JSON file.
+'''
+Gathers data from an API and exports it to a JSON file.
 '''
 import json
 import requests
@@ -10,21 +11,21 @@ API_URL = 'https://jsonplaceholder.typicode.com'
 if __name__ == '__main__':
     # Fetch user data from the API
     users_res = requests.get(f'{API_URL}/users').json()
-    
+
     # Fetch todos data from the API
     todos_res = requests.get(f'{API_URL}/todos').json()
-    
+
     # Initialize a dictionary to store user data
     users_data = {}
-    
+
     # Iterate through users
     for user in users_res:
         id = user.get('id')
         user_name = user.get('username')
-        
+
         # Filter todos for the given user ID
         todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-        
+
         # Create a list of user data (tasks and completion status)
         user_data = list(map(
             lambda x: {
@@ -34,10 +35,10 @@ if __name__ == '__main__':
             },
             todos
         ))
-        
+
         # Add user data to the dictionary
         users_data[f'{id}'] = user_data
-    
+
     # Export the user data to a JSON file
     with open('todo_all_employees.json', 'w') as file:
         json.dump(users_data, file)
