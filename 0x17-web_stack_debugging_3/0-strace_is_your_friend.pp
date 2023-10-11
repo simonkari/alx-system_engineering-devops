88671a3c0dbe061510afd4ca8c2de1a5a527ed12
simonkari <kariithisimon1715@gmail.com>
2023-10-11 19:15:30 +0300

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# Fixes a faulty wordpress site
-exec { 'fix-wordpress':
+# Puppet Manifest to Fix a Faulty WordPress Site
+exec { 'fix-wordpress error':
+  # Specify the shell command to be executed
   command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
 /var/www/html/wp-settings.php; service apache2 restart"',
   path    => '/usr/bin:/usr/sbin:/bin'
