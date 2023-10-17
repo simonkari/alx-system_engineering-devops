59bb2eaf262a83ae116547a29370e3d57e2ef225
simonkari <kariithisimon1715@gmail.com>
2023-10-17 21:46:02 +0300

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# Corrects an issue with an nginx site that
+struggles with handling multiple simultaneous requests
+exec { 'fix-for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/'
+  /etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
